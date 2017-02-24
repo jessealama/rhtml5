c2a744da545d550d366faf21675ae4885e9af45a
Jesse Alama <jesse@Jesses-MacBook.fritz.box>
2017-02-24 07:34:23 +0100

    Re-enable tests, and test for unusual whitespace characters.

--- encoding.rkt
+++ encoding.rkt
@@ -28,45 +28,24 @@
                                sp/byte))
 
 (define (sniff-encoding/bytes bytestr)
-  (define length (bytes-length bytestr))
-  (define initial-segment-length (min 1024 length))
+  (define blen (bytes-length bytestr))
+  (define initial-segment-length (min 1024 blen))
   (define initial-segment (subbytes bytestr
                                     0
                                     initial-segment-length))
   (define (get-attribute pos)
     (values #"q" #"frob" (+ pos 1)))
   (define (encoding-from-meta pos)
-    (define got-pragma? #f)
-    (define need-pragma? #f)
-    (define finished? #f)
-    (define encoding #f)
-    (define (foo foo-pos)
-      (let-values ([(name value next-pos) (get-attribute foo-pos)])
-        (if (and (eq? name #f)
-                 (eq? value #f))
-            (if (eq? need-pragma? #f)
-                (sniff (+ pos 1))
-                (if (eq? got-pragma? #f)
-                    (sniff (+ pos 1))
-                    (sniff (+ pos 1))))
-            (cond ((bytes=? name #"http-equiv")
-                   (set! got-pragma? #t))
-                  ((bytes=? name #"content")
-                   (let ([encoding-from-meta (encoding-from-meta value)])
-                     (when encoding-from-meta
-                       (unless encoding
-                         (set! encoding encoding-from-meta)
-                         (set! need-pragma? #t)))))
-                  ((bytes=? name #"charset")
-                   (set! encoding value)
-                   (set! need-pragma? #f))))))
-    (foo pos))
+    "iso-8859-1")
   (define (sniff pos)
-    (let ([meta-match (regexp-match "<[mM][eE][tT][aA][ /](.*)" initial-segment pos)])
+    (let ([meta-match (regexp-match "^<[mM][eE][tT][aA][ /](.*)"
+                                    initial-segment
+                                    pos)])
       (if (and (list? meta-match)
                (= (length meta-match) 2)
-               (bytes? (list-ref meta-match 2)))
-          (encoding-from-meta (+ pos 7))
+               (bytes? (list-ref meta-match 1)))
+          (values (encoding-from-meta (+ pos 7))
+                  'certain)
           (values "utf-8"
                   'certain))))
   (sniff 0))
@@ -120,18 +99,18 @@
   (check-exn exn:fail?
              (lambda ()
                (sniff-encoding "" #:encoding 5)))
-  ;; (test-html #f
-  ;;            "iso-8859-1"
-  ;;            "iso-8859-1"
-  ;;            'certain)
-  ;; (test-html #""
-  ;;            #f
-  ;;            "utf-8"
-  ;;            'tentative)
-  ;; (test-html #"<!--<meta charset=\"iso-8859-1\"/>"
-  ;;            #f
-  ;;            "iso-8859-1"
-  ;;            'certain)
+  (test-html #f
+             "iso-8859-1"
+             "iso-8859-1"
+             'certain)
+  (test-html #""
+             #f
+             "utf-8"
+             'tentative)
+  (test-html #"<!--<meta charset=\"iso-8859-1\"/>"
+             #f
+             "iso-8859-1"
+             'certain)
   (test-html #"<html><body><p>hi"
              #f
              "utf-8"
@@ -140,8 +119,41 @@
              #f
              "utf-8"
              'certain)
-  ;; (test-html #"<meta charset=\"iso-8859-4\"/>"
-  ;;            #f
-  ;;            "iso-8859-4"
-  ;;            'certain)
-  )
+  (test-html #"<meta charset=\"iso-8859-4\"/>"
+             #f
+             "iso-8859-4"
+             'certain)
+
+  ;; The next group of tests check whether we correctly handle encoding detection
+  ;; when the character after "<meta" is not a simple space.
+  (test-html (bytes-append #"<meta"
+                           (string->bytes/utf-8 (format "~c" #\u0009)) ;; tab
+                           #"charset=\"iso-8859-9\"/>")
+             #f
+             "iso-8859-9"
+             'certain)
+  (test-html (bytes-append #"<meta"
+                           (string->bytes/utf-8 (format "~c" #\u000A)) ;; line feed
+                           #"charset=\"iso-885-3\"/>")
+             #f
+             "iso-885-3"
+             'certain)
+  (test-html (bytes-append #"<meta"
+                           (string->bytes/utf-8 (format "~c" #\u000C)) ;; form feed
+                           #"charset=\"iso-884-3\"/>")
+             #f
+             "iso-884-3"
+             'certain)
+  (test-html (bytes-append #"<meta"
+                           (string->bytes/utf-8 (format "~c" #\u000D)) ;; carriage return
+                           #"charset=\"iso-894-3\"/>")
+             #f
+             "iso-894-3"
+             'certain)
+  ;; here's a whitespace character that, officially, is not supported
+  (test-html (bytes-append #"<meta"
+                           (string->bytes/utf-8 (format "~c" #\u000B)) ;; vertical tab
+                           #"charset=\"iso-894-3\"/>")
+             #f
+             "utf-8"
+             'tentative))
