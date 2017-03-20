f43d836957e8bbe567716a9dc0e4dc1a62572fd1
Jesse Alama <jesse@racket.sh>
2017-03-20 09:37:48 +0100

    Factor out name and attribute functions.

--- attrs.rkt
+++ attrs.rkt
@@ -6,48 +6,50 @@
 
 (define (consume-spaces bstr pos)
   (define blen (bytes-length bstr))
-  (if (> (+ pos 1) blen)
-      #f
+  (if (<= blen pos)
+      pos
       (let ([b (bytes-ref bstr pos)])
         (cond ([whitespace-byte? b]
                (consume-spaces bstr (+ pos 1)))
               ((not (= b =/byte))
-               #f)
+               pos)
               (else
                (+ pos 1))))))
 
-(define (attr-name+value bstr pos)
-  (define blen (bytes-length bstr))
-  (define (value-helper pos attr-value)
-    "blog")
+(define (extract-value bstr pos)
+  (let ([v "blog"])
+    (values v (+ pos (string-length v)))))
+
+(define (extract-name bstr pos)
   (define (name-helper pos attr-name)
-    (if (> (+ pos 1) blen)
-        (values attr-name "")
-        (let ([b (bytes-ref bstr pos)])
-          (cond ((and (= b =/byte)
-                      (not (string=? attr-name "")))
-                 (values attr-name
-			 (value-helper (+ pos 1) "")))
-                ((whitespace-byte? b)
-                 (let ([new-pos (consume-spaces bstr (+ pos 1))])
-                   (values attr-name
-                           (if (integer? new-pos)
-                               (value-helper (+ new-pos 1) "")
-                               ""))))
-                ((or (= b slash/byte)
-                     (= b >/byte))
-                 (values attr-name ""))
-                ((and (>= b 65) (<= b 90))
-                 (name-helper (+ pos 1)
+    (if (<= (bytes-length bstr) pos)
+	(values attr-name pos)
+	(let ([b (bytes-ref bstr pos)])
+	  (cond ((and (= b =/byte)
+		      (not (string=? attr-name "")))
+		 (values attr-name (+ pos 1)))
+		((whitespace-byte? b)
+		 (let ([new-pos (consume-spaces bstr (+ pos 1))])
+		   (values attr-name new-pos)))
+		((or (= b slash/byte)
+		     (= b >/byte))
+		 (values attr-name (+ pos 1)))
+		((and (>= b 65) (<= b 90))
+		 (name-helper (+ pos 1)
 			      (format "~a~a"
 				      attr-name
 				      (byte->string/utf-8 (+ b 32)))))
-                (else
-                 (name-helper (+ pos 1)
+		(else
+		 (name-helper (+ pos 1)
 			      (format "~a~a"
 				      attr-name
 				      (byte->string/utf-8 b))))))))
   (name-helper pos ""))
+
+(define (attr-name+value bstr pos)
+  (let-values ([(attr after-name-pos) (extract-name bstr pos)])
+    (let-values ([(value after-value-pos) (extract-value bstr after-name-pos)])
+      (values attr value after-value-pos))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Tests
@@ -56,16 +58,17 @@
 (module+ test
   (require rackunit)
 
-  (define (test-attrs input-bytes position expected-attr-name expected-attr-value)
-    (let-values ([(n v) (attr-name+value input-bytes position)])
+  (define (test-attrs input-bytes position expected-attr-name expected-attr-value expected-position)
+    (let-values ([(n v pos) (attr-name+value input-bytes position)])
       (check-equal? n expected-attr-name)
-      (check-equal? v expected-attr-value))))
+      (check-equal? v expected-attr-value)
+      (check-equal? pos expected-position))))
 
 (module+ test
   (define bstr-1 #"a=b")
-  (test-attrs bstr-1 0 "a" "b")
-  (test-attrs bstr-1 1 #f #f)
+  (test-attrs bstr-1 0 "a" "b" 3)
+  (test-attrs bstr-1 1 #f #f 3)
   (define bstr-2 #" xy = z")
-  (test-attrs bstr-2 0 "xy" "z")
-  (test-attrs bstr-2 1 "xy" "z")
+  (test-attrs bstr-2 0 "xy" "z" 7)
+  (test-attrs bstr-2 1 "xy" "z" 7)
 )
