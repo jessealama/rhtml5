80d6ab0e044e35cf0478d79f4b4032bfc25884d9
Jesse Alama <jesse@racket.sh>
2017-03-25 07:38:29 +0100

    Testing bytes.

--- bytes.rkt
+++ bytes.rkt
@@ -8,20 +8,24 @@
 (define !/byte (char->integer #\u0021))	    ;; !
 (define -/byte (char->integer #\u002D))	    ;; -
 (define =/byte (char->integer #\u003D))	    ;; =
-(define m/byte (char->integer #\u004D))     ;; m
-(define M/byte (char->integer #\u006D))	    ;; M
+(define m/byte (char->integer #\u006D))     ;; m
+(define M/byte (char->integer #\u004D))	    ;; M
 (define e/byte (char->integer #\u0045))	    ;; e
 (define E/byte (char->integer #\u0065))	    ;; E
 (define t/byte (char->integer #\u0054))	    ;; t
 (define T/byte (char->integer #\u0074))     ;; T
-(define a/byte (char->integer #\u0041))	    ;; a
-(define A/byte (char->integer #\u0061))	    ;; A
+(define a/byte (char->integer #\u0061))	    ;; a
+(define A/byte (char->integer #\u0041))	    ;; A
 (define tab/byte (char->integer #\u0009))   ;; tab
 (define lf/byte (char->integer #\u000A))    ;; line feed
 (define ff/byte (char->integer #\u000C))    ;; form feed
 (define cr/byte (char->integer #\u000D))    ;; carriage return
 (define sp/byte (char->integer #\u0020))    ;; space
 (define slash/byte (char->integer #\u002F)) ;; slash
+(define double-quote/byte (char->integer #\u0022)) ;; double quote
+(define single-quote/byte (char->integer #\u0027)) ;; single quote
+(define Z/byte (char->integer #\u005A)) ;; Z
+
 
 (define (whitespace-byte? b)
   (or (= b tab/byte)
@@ -29,6 +33,17 @@
       (= b ff/byte)
       (= b cr/byte)
       (= b sp/byte)))
+
+(define (quote-byte? b)
+  (or (= b single-quote/byte)
+      (= b double-quote/byte)))
+
+(define (uppercase-letter? b)
+  (and (>= b A/byte)
+       (<= b Z/byte)))
+
+(define (lowercase-byte b)
+  (+ b 32))
 
 (define (byte->string/utf-8 byte)
   ;; no check that byte really is a valid UTF-8 character, or that, even
@@ -43,4 +58,9 @@
 (module+ test
   (require rackunit))
 
-(module+ test)
+(module+ test
+  (check-true (uppercase-letter? (bytes-ref #"A" 0)))
+  (check-false (uppercase-letter? (bytes-ref #"f" 0)))
+  (check-false (uppercase-letter? (bytes-ref #"?" 0)))
+  (check-equal? (lowercase-byte (bytes-ref #"A" 0))
+		(bytes-ref #"a" 0)))
