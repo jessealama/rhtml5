fbbac9b8badb1875abdef8f81b099cc7a5c33c3c
Jesse Alama <jesse@racket.sh>
2017-03-26 14:23:18 +0200

    More refactoring.

--- attrs.rkt
+++ attrs.rkt
@@ -16,54 +16,49 @@
               (else
                (+ pos 1))))))
 
-(define (extract-value bstr pos)
-  (define blen (bytes-length bstr))
-  (define (extract-value-helper pos quote-char attr-value)
-    (if (<= pos blen)
-	attr-value
-	(let ([b (bytes-ref bstr pos)])
-	  (cond ([whitespace-byte? b]
-		 (extract-value-helper (+ pos 1) quote-char attr-value))
-		([(quote-byte? b)]
-		 (if (byte? quote-char)
-		     (values attr-value (+ pos 1))
-		     (extract-value-helper (+ pos 1) b attr-value)))
-		([uppercase-letter? b]
-		 (extract-value-helper (+ pos 1) quote-char (format "~a~a" attr-value (lowercase-byte b))))
-		(else
-		 (extract-value-helper (+ pos 1) quote-char (format "~a~a" attr-value b)))))))
-  (extract-value-helper pos #f ""))
-
-(define (extract-name bstr pos)
-  (define (name-helper pos attr-name)
-    (if (<= (bytes-length bstr) pos)
-	(values attr-name pos)
-	(let ([b (bytes-ref bstr pos)])
-	  (cond ((and (= b =/byte)
-		      (not (string=? attr-name "")))
-		 (values attr-name (+ pos 1)))
-		((whitespace-byte? b)
-		 (let ([new-pos (consume-spaces bstr (+ pos 1))])
-		   (values attr-name new-pos)))
-		((or (= b slash/byte)
-		     (= b >/byte))
-		 (values attr-name (+ pos 1)))
-		((and (>= b 65) (<= b 90))
-		 (name-helper (+ pos 1)
-			      (format "~a~a"
-				      attr-name
-				      (byte->string/utf-8 (+ b 32)))))
-		(else
-		 (name-helper (+ pos 1)
-			      (format "~a~a"
-				      attr-name
-				      (byte->string/utf-8 b))))))))
-  (name-helper pos ""))
-
+;; byte-string integer -> string|false string|false integer
 (define (attr-name+value bstr pos)
-  (let-values ([(attr after-name-pos) (extract-name bstr pos)])
-    (let-values ([(value after-value-pos) (extract-value bstr after-name-pos)])
-      (values attr value after-value-pos))))
+  (define len (bytes-length bstr))
+  (define (extract-quoted-value pos quote-char attr-value)
+    (values "blug" pos))
+  ;; integer string -> string integer
+  (define (extract-value pos quote-char attr-value)
+    (if (<= len pos)
+	(values "" pos)
+	(let ([b (bytes-ref bstr pos)])
+	  (cond
+	   ((whitespace-byte? b)
+	    (extract-value (+ pos 1) quote-char attr-value))
+	   ((quote-byte? b)
+	    (extract-quoted-value b attr-value))
+	   (else
+	    (values "whatever" (+ pos 1)))))))
+  ;; integer -> string string integer
+  (define (extract-name+value pos attr-name)
+    (if (<= len pos)
+	(values attr-name "" pos)
+	(let ([b (bytes-ref bstr pos)])
+	  (cond
+	   ([= b >/byte]
+	    (values #f #f pos))
+	   ((and (= b =/byte)
+		 (string? attr-name)
+		 (not (string=? attr-name "")))
+	    (let-values ([(val pos) (extract-value (+ pos 1) #f "")])
+	      (values attr-name val pos)))
+	   ((whitespace-byte? b)
+	    (extract-name+value (+ pos 1) attr-name))
+	   ((or (= b slash/byte)
+		(= b >/byte))
+	    (values attr-name "" (+ pos 1)))
+	   (else
+	    (extract-name+value (+ pos 1)
+				(format "~a~a"
+					attr-name
+					(byte->string/utf-8 (if (uppercase-letter? b)
+								(+ b 32)
+								b)))))))))
+  (extract-name+value pos ""))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Tests
